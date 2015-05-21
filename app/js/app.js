c2e49393e30cffdfba6436de926807c0f99961fc
yuletodim <yuletodim@users.noreply.github.com>
2015-05-21 19:01:07 +0300

    Added initial routing. Welcome screen made. Fixed index.html.

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,14 @@
-var app = angular.module('SocialNetworkApp', []);
+var app = angular.module('SocialNetworkApp', ['ngRoute', 'ngResource']);
+
+app.constant('baseServiceUrl', 'http://softuni-social-network.azurewebsites.net/api ');
+
+app.config(function ($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl: 'templates/welcome.html',
+		controller: 'HomeController'
+	});
+
+	$routeProvider.otherwise(
+		{ redirectTo: '/' }
+	);
+});
