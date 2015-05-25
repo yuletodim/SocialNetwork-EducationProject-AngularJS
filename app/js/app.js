8d673319ec2ca8d1bbf361051c0e322ed8d18449
yuletodim <yuletodim@users.noreply.github.com>
2015-05-25 16:13:13 +0300

    Added bootstrap-theme. Made login.html, register.html, footer.html. Made header and home page for not-registered users. Bug fixed.

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,13 @@
+'use strict';
+
 var app = angular.module('SocialNetworkApp', ['ngRoute', 'ngResource']);
 
-app.constant('baseServiceUrl', 'http://softuni-social-network.azurewebsites.net/api ');
+app.constant('baseServiceUrl', 'http://softuni-social-network.azurewebsites.net');
 
 app.config(function ($routeProvider) {
 	$routeProvider.when('/', {
-		templateUrl: 'templates/welcome.html',
-		controller: 'HomeController'
+		templateUrl: 'templates/home.html',
+		//controller: 'HomeController'
 	});
 
 	$routeProvider.otherwise(
--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -1,5 +1,9 @@
-app.controller('MainController', function ($scope) {
+'use strict';
+
+app.controller('MainController', function ($scope, authService) {
 	$scope.welcomeMessage = "Welcome to the Project!";
 
-	
+	// make authService available from every point of the app
+	//$scope.authService = authService;
+
 });
